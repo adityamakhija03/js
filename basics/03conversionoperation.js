62c7beee294d47086997c133841c2545b339d275
adityamakhija03 <105123484+adityamakhija03@users.noreply.github.com>
2023-01-01 22:33:36 +0530

    Add files via upload

---
+++ basics/03conversionoperation.js
@@ -0,0 +1,49 @@
+// let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score));
+// //const {score}=req.body
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber)
+
+
+
+
+
+
+
+ //let score =null
+// let score=undefined
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+
+
+
+// let isLoggedIn="turre"
+//  let booleanisLoggedIn=Boolean(isLoggedIn)
+
+//  console.log(booleanisLoggedIn)
+
+
+
+let num=33
+let stringnum=String(num)
+console.log( typeof stringnum);
+
+
+
+
+//note --
+/*
+1. "33" in number=>33
+2. "33abc" in number=>NaN
+3. true=>1; false=>0 
+4. 0 in boolean =>false
+5. any number than zero OR  any non empty string in boolean =>true
+6.""(empty strings) in boolean => false 
+
+*/
